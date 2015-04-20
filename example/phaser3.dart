bebf3ab3a3cef4ac3032304b157ed935d686dc68
oxysoft <teoxysoft@gmail.com>
2015-04-20 14:51:13 -0400

    Initial commit

---
+++ example/phaser3.dart
@@ -0,0 +1,11 @@
+// Copyright (c) 2015, <your name>. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+library phaser3.example;
+
+import 'package:phaser3/phaser3.dart';
+
+main() {
+  var awesome = new Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
